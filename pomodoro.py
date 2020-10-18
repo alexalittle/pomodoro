551bbed6add52503d32791789d1db9a043db8e33
Alexa Little <alittle@7000.org>
2020-10-17 23:47:08 -0400

    Implement Pomodoro function and Skip button
    Removed buttons to add/subtract time -- will implement later as settings if needed.
    
    Created pomodoro function to automatically set pomodoro timers.
    
    Changed "Reset" button to "Restart" current pomodoro timer.
    
    Implemented "Skip" button to skip to next pomodoro timer.

--- pomodoro.py
+++ pomodoro.py
@@ -5,6 +5,12 @@ import tkinter as tk
 class App:
 	def __init__(self, master):
 
+		# pomodoro settings
+		self.work_time = 25 * 60
+		self.short_break_time = 5 * 60
+		self.long_break_time = 25 * 60
+		self.pomodoro_status = 1
+
 		# set attributes for start time, current time
 		self.start_time = 0
 		self.current_time = 0
@@ -18,48 +24,28 @@ class App:
 		title_frame.pack(side = tk.TOP)
 
 		# set up main display
-		self.title = tk.Label(title_frame, text = "Timer", font=(None, 16))
+		self.title = tk.Label(title_frame, text = "Pomodoro Timer", font=(None, 16))
 		self.title.pack()
-		self.timer_display = tk.Label(title_frame, text = "0:00", font=(None, 24))
+		self.timer_display = tk.Label(title_frame, text = "25:00", font=(None, 24))
 		self.timer_display.pack()
 
 		# create section for control buttons
 		control_frame = tk.Frame(master)
 		control_frame.pack(side = tk.TOP)
 
-		# create start, stop, reset, clear buttons
-		self.start_btn = tk.Button(control_frame, text="Start", command = self.start_handler)
+		# create start, stop, skip, reset, clear buttons
+		self.start_btn = tk.Button(control_frame, text="Start", command = self.do_pomodoro)
 		self.start_btn.pack(side = tk.LEFT)
 		self.stop_btn = tk.Button(control_frame, text="Stop", command = self.stop_timer)
 		self.stop_btn.pack(side = tk.LEFT)
-		self.reset_btn = tk.Button(control_frame, text="Reset", command = self.reset_timer)
+		self.skip_btn = tk.Button(control_frame, text="Skip", command = self.skip_session)
+		self.skip_btn.pack(side = tk.LEFT)
+		self.reset_btn = tk.Button(control_frame, text="Reset", command = self.restart_timer)
 		self.reset_btn.pack(side = tk.LEFT)
 		self.clear_btn = tk.Button(control_frame, text="Clear", command = self.clear_timer)
 		self.clear_btn.pack(side = tk.LEFT)
 
-		# create section for adding minutes
-		minutes_frame = tk.Frame(master)
-		minutes_frame.pack()
-
-		# create buttons for adding minutes
-		self.minutes_label = tk.Label(minutes_frame, text = "Add Minutes")
-		self.minutes_label.pack(side = tk.TOP)
-		self.minutes_minus_btn = tk.Button(minutes_frame, text = "-", command = lambda: self.change_time(-60))
-		self.minutes_minus_btn.pack(side = tk.LEFT)
-		self.minutes_plus_btn = tk.Button(minutes_frame, text = "+", command = lambda: self.change_time(60))
-		self.minutes_plus_btn.pack(side = tk.RIGHT)
-
-		# create section for adding seconds
-		seconds_frame = tk.Frame(master)
-		seconds_frame.pack()
-
-		#create section for adding seconds
-		self.seconds_label = tk.Label(seconds_frame, text = "Add Seconds")
-		self.seconds_label.pack(side = tk.TOP)
-		self.seconds_minus_btn = tk.Button(seconds_frame, text = "-", command = lambda: self.change_time(-1))
-		self.seconds_minus_btn.pack(side = tk.LEFT)
-		self.seconds_plus_btn = tk.Button(seconds_frame, text = "+", command = lambda: self.change_time(1))
-		self.seconds_plus_btn.pack(side = tk.RIGHT)
+		# other controls will go here
 
 	def update_display(self):
 		""" Updates the timer on the app display."""
@@ -79,18 +65,28 @@ class App:
 				self.callback = root.after(1000, lambda: self.start_timer())
 			else:
 				root.bell()
+				self.pomodoro_status += 1
 				self.callback = None
+				self.do_pomodoro()
 
 	def stop_timer(self):
 		""" Stops the timer."""
 		if self.callback:
 			root.after_cancel(self.callback)
 			self.callback = None
+			self.start_btn["text"] = "Resume"
 
-	def reset_timer(self):
+	def skip_session(self):
+		root.after_cancel(self.callback)
+		self.callback = None
+		self.pomodoro_status += 1
+		self.do_pomodoro()
+
+	def restart_timer(self):
 		""" Stops the timer if running and resets it to the start time value."""
 		self.stop_timer()
 		self.current_time = self.start_time
+		self.start_btn["text"] = "Start"
 		self.update_display()
 
 	def clear_timer(self):
@@ -98,16 +94,8 @@ class App:
 		self.stop_timer()
 		self.current_time = 0
 		self.start_time = 0
+		self.start_btn["text"] = "Start"
 		self.update_display()
-
-	def change_time(self, num):
-		""" Given an input in seconds, adds that value to the current time. Prevents negative time values."""
-		if self.current_time + num >= 0:
-			self.current_time += num
-			# changes made while timer is already running won't affect saved "start time" value
-			if not self.callback:
-				self.start_time += num
-			self.update_display()
 
 	def sec_to_min(self, seconds):
 		""" Given input in seconds, returns equivalent value in minutes (0:00 string)."""
@@ -118,9 +106,29 @@ class App:
 			output = str(calculation[1])
 		return str(calculation[0]) + ":" + output
 
+	def do_pomodoro(self):
+		if self.pomodoro_status % 2 == 1:
+			# odd numbers = do pomodoro work time
+			timer_len = self.work_time
+			title_value = "Work Session"
+		elif self.pomodoro_status % 8 == 0:
+			# after 4 work sessions and 3 breaks, take a long break
+			timer_len = self.long_break_time
+			title_value = "Long Break"
+		else:
+			# even numbers = do pomodoro break time
+			timer_len = self.short_break_time
+			title_value = "Short Break"
+			# do long break
+		self.start_time = timer_len
+		self.current_time = timer_len
+		self.title["text"] = title_value
+		self.update_display()
+		self.start_handler()
+
 root = tk.Tk()
 root.geometry("300x200")
-root.title("Timer")
+root.title("Pomodoro Timer")
 app = App(root)
 
 root.mainloop()
