76316869b11738da45b4a3db96389463d0d76bb2
Alexa Little <alittle@7000.org>
2020-10-18 21:53:20 -0400

    Updates - color display, continuous timers
    Added color change display (black at start, red for work, green for breaks).
    
    Changed pomodoro timers so that work/break timer sessions run continuously (unless the user pauses).
    
    Still bugfixing issue with "is_paused."

--- pomodoro.py
+++ pomodoro.py
@@ -5,11 +5,19 @@ import tkinter as tk
 class App:
 	def __init__(self, master):
 
+		super().__init__()
+		self.master = master
+
 		# pomodoro settings
-		self.work_time = 25 * 60
-		self.short_break_time = 5 * 60
-		self.long_break_time = 25 * 60
+		self.work_time = 5 #25 * 60
+		self.short_break_time = 5 # 5 * 60
+		self.long_break_time = 5 # 25 * 60
 		self.pomodoro_status = 1
+
+		# default display attributes - background color, window size
+		self.color = "black"
+		master['bg'] = self.color
+		master.geometry("300x200")
 
 		# set attributes for start time, current time
 		self.start_time = 0
@@ -21,13 +29,14 @@ class App:
 		self.callback = None
 
 		# create section for main display
-		title_frame = tk.Frame(master)
-		title_frame.pack(side = tk.TOP)
+		self.title_frame = tk.Frame(master)
+		self.title_frame.pack(side = tk.TOP)
+		self.title_frame['bg'] = self.color
 
 		# set up main display
-		self.title = tk.Label(title_frame, text = "Pomodoro Timer", font=(None, 16))
+		self.title = tk.Label(self.title_frame, text = "Pomodoro Timer", font=(None, 16), background=self.color, foreground='white')
 		self.title.pack()
-		self.timer_display = tk.Label(title_frame, text = "25:00", font=(None, 24))
+		self.timer_display = tk.Label(self.title_frame, text = "25:00", font=(None, 24), background=self.color, foreground='white')
 		self.timer_display.pack()
 
 		# create section for control buttons
@@ -46,11 +55,17 @@ class App:
 		self.clear_btn = tk.Button(control_frame, text="Clear", command = self.clear_timer)
 		self.clear_btn.pack(side = tk.LEFT)
 
-		# other controls will go here
-
-	def update_display(self):
+	def update_timer(self):
 		""" Updates the timer on the app display."""
 		self.timer_display["text"] = self.sec_to_min(self.current_time)
+
+	def update_colors(self, master, color):
+		""" Updates the background color of the app window."""
+		self.color = color
+		self.master['bg'] = self.color
+		self.title_frame["background"] = self.color
+		self.title['bg'] = self.color
+		self.timer_display['bg'] = self.color
 
 	def start_handler(self):
 		""" Helper function.
@@ -70,14 +85,14 @@ class App:
 		""" Starts the timer."""
 		if self.start_time > 0 and self.current_time >= 1:
 			self.current_time -= 1
-			self.update_display()
+			self.update_timer()
 			if self.current_time > 0:
 				self.callback = root.after(1000, lambda: self.start_timer())
 			else:
 				root.bell()
 				self.pomodoro_status += 1
 				self.callback = None
-				self.set_pomodoro()
+				self.start_handler()
 
 	def stop_timer(self):
 		""" Stops the timer."""
@@ -88,6 +103,7 @@ class App:
 			self.start_btn["text"] = "Resume"
 
 	def skip_session(self):
+		""" Advances to the next session, with timer paused."""
 		if self.callback:
 			root.after_cancel(self.callback)
 			self.callback = None
@@ -99,15 +115,19 @@ class App:
 		self.stop_timer()
 		self.current_time = self.start_time
 		self.start_btn["text"] = "Start"
-		self.update_display()
+		self.update_timer()
 
 	def clear_timer(self):
-		""" Stops the timer if running and resets all time values to 0."""
+		""" Resets all pomodoro values to initial states."""
 		self.stop_timer()
-		self.current_time = 0
-		self.start_time = 0
+		self.pomodoro_status = 1
 		self.start_btn["text"] = "Start"
-		self.update_display()
+		self.start_time = self.work_time
+		self.update_display(self.start_time, "Pomodoro Timer", "black")
+		# temporary workaround:
+		""" after clear function, color & title changes don't work correctly when start is pressed again
+			==> this is caused by a problem in the is_paused loop logic of start_handler()"""
+		self.is_paused = False
 
 	def sec_to_min(self, seconds):
 		""" Given input in seconds, returns equivalent value in minutes (0:00 string)."""
@@ -123,22 +143,27 @@ class App:
 			# odd numbers = do pomodoro work time
 			timer_len = self.work_time
 			title_value = "Work Session"
+			color = "red"
 		elif self.pomodoro_status % 8 == 0:
 			# after 4 work sessions and 3 breaks, take a long break
 			timer_len = self.long_break_time
 			title_value = "Long Break"
+			color = "green"
 		else:
 			# even numbers = do pomodoro break time
 			timer_len = self.short_break_time
 			title_value = "Short Break"
-			# do long break
+			color = "green"
 		self.start_time = timer_len
+		self.update_display(timer_len, title_value, color)
+
+	def update_display(self, timer_len, title_value, color):
 		self.current_time = timer_len
 		self.title["text"] = title_value
-		self.update_display()
+		self.update_timer()
+		self.update_colors(self.master, color)
 
 root = tk.Tk()
-root.geometry("300x200")
 root.title("Pomodoro Timer")
 app = App(root)
 
